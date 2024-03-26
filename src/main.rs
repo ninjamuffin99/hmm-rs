5c6c5a6f032c6327e6056e0fb9cda5fe3da7c686
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2024-03-26 16:39:23 -0400

    code formatting

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
+use clap::{Parser, Subcommand};
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
+use std::fmt;
 use std::fs::File;
 use std::io::prelude::*;
-use std::fmt;
-use clap::{Parser, Subcommand};
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[command(subcommand)]
-    cmd: Commands
+    cmd: Commands,
 }
 
 #[derive(Subcommand, Debug, Clone)]
 enum Commands {
     List {
         #[arg(short, long)]
-        path: Option<PathBuf>
-    }
+        path: Option<PathBuf>,
+    },
 }
 
 #[derive(Serialize, Deserialize)]
@@ -45,8 +45,6 @@ impl fmt::Display for Dependancies {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", serde_json::to_string_pretty(self).unwrap())
     }
-
-    
 }
 
 impl Dependancies {
@@ -56,23 +54,26 @@ impl Dependancies {
                 Some(v) => format!("version: {}", v),
                 None => match &haxelib.vcs_ref {
                     Some(r) => format!("ref: {}", r),
-                    None => format!("No version or ref")
-                }
+                    None => format!("No version or ref"),
+                },
             };
 
-            let mut haxelib_output = format!("{} [{haxelib_type}] \n{} \n", haxelib.name, version_or_ref, haxelib_type = haxelib.haxelib_type);
-            
+            let mut haxelib_output = format!(
+                "{} [{haxelib_type}] \n{} \n",
+                haxelib.name,
+                version_or_ref,
+                haxelib_type = haxelib.haxelib_type
+            );
+
             match haxelib.haxelib_type.as_str() {
-                "git" => {
-                    match &haxelib.url {
-                        Some(u) => haxelib_output.push_str(&format!("url: {}\n", u)),
-                        None => {}
-                    }
+                "git" => match &haxelib.url {
+                    Some(u) => haxelib_output.push_str(&format!("url: {}\n", u)),
+                    None => {}
                 },
                 "haxelib" => {
                     let haxelib_url = format!("https://lib.haxe.org/p/{}", haxelib.name);
                     haxelib_output.push_str(&format!("url: {}\n", haxelib_url))
-                },
+                }
                 _ => {}
             }
 
@@ -82,20 +83,18 @@ impl Dependancies {
 }
 
 fn main() {
-    
     // println!("Hello, world!");
 
-    
     match_commands();
 
     // let dep = print_flixel_haxelib().unwrap();
     // println!("{}", dep.to_string());
     // save_json(dep, "samples/flixel.json").unwrap();
-    
+
     // match read_json("samples/hmm.json") {
     //     Ok(dep_read) => {
     //         println!("Read: {}", dep_read.to_string());
-    //     }, 
+    //     },
     //     Err(e) => {
     //         println!("Error: {:?}", e);
     //     }
@@ -110,26 +109,23 @@ fn match_commands() {
     let args = Args::parse();
     match args.cmd {
         Commands::List { path } => {
-            
             let file_to_open = match path {
                 Some(dir_path) => dir_path,
-                None => "hmm.json".into()
+                None => "hmm.json".into(),
             };
 
             match read_json(file_to_open.to_str().unwrap()) {
                 Ok(dep_read) => {
                     dep_read.print_string_list();
-                }, 
+                }
                 _ => {}
             }
         }
     }
-
 }
 
 fn read_json(path: &str) -> std::io::Result<Dependancies> {
-    let file = match File::open(path)
-    {
+    let file = match File::open(path) {
         Ok(file) => file,
         Err(e) => {
             println!("Error: {:?} not found", path);
@@ -149,7 +145,6 @@ fn save_json(deps: Dependancies, path: &str) -> std::io::Result<()> {
 }
 
 fn print_flixel_haxelib() -> Result<Dependancies> {
-    
     let haxelib = Haxelib {
         name: String::from("flixel"),
         haxelib_type: String::from("git"),
@@ -159,7 +154,7 @@ fn print_flixel_haxelib() -> Result<Dependancies> {
         version: Option::None,
     };
 
-    let dep:Dependancies = Dependancies {
+    let dep: Dependancies = Dependancies {
         dependencies: vec![haxelib],
     };
 
