3ac5005cad95647cea40a3b155f91d6cdbb48d56
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2024-04-23 02:58:43 -0400

    gix / git installs in progress

--- src/commands/install_command.rs
+++ src/commands/install_command.rs
@@ -1,15 +1,23 @@
 use crate::hmm;
 use crate::hmm::haxelib::Haxelib;
 use crate::hmm::haxelib::HaxelibType;
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
+use bstr::BString;
 use console::Emoji;
 use futures_util::StreamExt;
+use gix::clone;
+use gix::create;
+use gix::progress::Discard;
+use gix::NestedProgress;
+use gix::Progress;
+use gix::Url;
 use human_bytes::human_bytes;
 use indicatif::{ProgressBar, ProgressStyle};
 use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
+use std::sync::atomic::AtomicBool;
 use yansi::Paint;
 use zip::ZipArchive;
 
@@ -19,6 +27,7 @@ pub fn install_from_hmm() -> Result<()> {
     for haxelib in deps.dependencies.iter() {
         match &haxelib.haxelib_type {
             HaxelibType::Haxelib => install_from_haxelib(haxelib)?,
+            HaxelibType::Git => install_from_git_using_gix(haxelib)?,
             lib_type => println!(
                 "{}: Installing from {:?} not yet implemented",
                 haxelib.name.red(),
@@ -26,6 +35,53 @@ pub fn install_from_hmm() -> Result<()> {
             ),
         }
     }
+
+    Ok(())
+}
+
+pub fn install_from_git_using_gix(haxelib: &Haxelib) -> Result<()> {
+    println!("Installing {} from git using gix", haxelib.name);
+
+    let path_with_no_https = haxelib.url.as_ref().unwrap().replace("https://", "");
+
+    let mut clone_url = Url::from_parts(
+        gix::url::Scheme::Https,
+        None,
+        None,
+        None,
+        None,
+        BString::from(path_with_no_https),
+        false,
+    )
+    .context(format!(
+        "error creating gix url for {}",
+        haxelib.url.as_ref().unwrap()
+    ))?;
+
+    let mut clone_path = PathBuf::from(".haxelib");
+    clone_path = clone_path.join(&haxelib.name).join("git");
+
+    std::fs::create_dir_all(&clone_path)?;
+
+    let opts = create::Options {
+        destination_must_be_empty: false,
+        fs_capabilities: None,
+    };
+
+    let mut da_fetch = clone::PrepareFetch::new(
+        clone_url,
+        clone_path,
+        create::Kind::WithWorktree,
+        opts,
+        gix::open::Options::default(),
+    )
+    .context("error preparing clone")?;
+
+    let mut da_checkout = da_fetch.fetch_then_checkout(Discard, &AtomicBool::new(false))?;
+
+    da_checkout
+        .0
+        .main_worktree(Discard, &AtomicBool::new(false))?;
 
     Ok(())
 }
@@ -83,6 +139,19 @@ pub async fn install_from_haxelib(haxelib: &Haxelib) -> Result<()> {
     let version_as_commas = haxelib.version.as_ref().unwrap().replace(".", ",");
     let mut output_dir: PathBuf = [".haxelib", haxelib.name.as_str()].iter().collect();
 
+    match std::fs::create_dir(&output_dir) {
+        Ok(_) => println!("Created directory: {:?}", output_dir.as_path()),
+        Err(e) => {
+            if e.kind() == std::io::ErrorKind::AlreadyExists {
+                println!("Directory already exists: {:?}", output_dir.as_path());
+            } else {
+                return Err(anyhow!(
+                    "Error creating directory: {:?}",
+                    output_dir.as_path()
+                ));
+            }
+        }
+    }
     // .current file
     println!(
         "Writing .current file to version: {:?}",
