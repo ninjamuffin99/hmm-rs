212ad5952d4414b7992352cee5f6b63e5e15c5d5
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2025-01-02 23:56:57 -0800

    haxelib install WORKING

--- src/commands/haxelib_command.rs
+++ src/commands/haxelib_command.rs
@@ -1,19 +1,53 @@
-use anyhow::Result;
+use anyhow::{anyhow, Ok, Result};
+use gix::actor::signature::decode;
+use reqwest::blocking::Client;
 
 use crate::{
     commands,
-    hmm::haxelib::{Haxelib, HaxelibType},
+    hmm::haxelib::{self, Haxelib, HaxelibType},
 };
 
 pub fn install_haxelib(name: &str, version: &Option<String>) -> Result<()> {
-    println!("Installing haxelib: {} {:?}", name, version);
-    let haxelib_install = Haxelib {
+    let mut haxelib_install = Haxelib {
         name: name.to_string(),
         haxelib_type: HaxelibType::Haxelib,
-        vcs_ref: version.clone(),
+        vcs_ref: None,
         dir: None,
         url: None,
-        version: version.clone(),
+        version: None,
+    };
+    match version {
+        Some(version) => haxelib_install.version = Some(version.to_string()),
+
+        None => {
+            // we need to query the latest version from haxelib
+            // haxelib url: lib.haxe.org/api/3.0/index.n/
+            // needs X-Haxe-Remoting header
+            // and __x param with the query
+            // in __x param, we can query with something like
+            // ay3:apiy16:getLatestVersionhay4:limeh
+
+            let serialized = format!("ay3:apiy16:getLatestVersionhay{}:{}h", name.len(), name);
+            let client = Client::new();
+
+            let resp = client
+                .get("https://lib.haxe.org/api/3.0/index.n/")
+                .header("X-Haxe-Remoting", "1")
+                .query(&[("__x", serialized)])
+                .send()?;
+
+            let resp = resp.text()?;
+            let resp_splits = resp.split(":").collect::<Vec<&str>>();
+            let decoded_resp = urlencoding::decode(resp_splits[1])?;
+
+            println!("Latest version of {} is {}", name, decoded_resp);
+
+            if (decoded_resp.starts_with("No such Project")) {
+                return Err(anyhow!("{}", decoded_resp)); // this haxelib doesn't exist
+            }
+
+            haxelib_install.version = Some(decoded_resp.to_string());
+        }
     };
     commands::install_command::install_from_haxelib(&haxelib_install)?;
     Ok(())
